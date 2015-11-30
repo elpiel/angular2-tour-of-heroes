f52fc68d26afffaedab10287172b5e360831bac8
Lachezar Lechev <luudman@gmail.com>
2015-11-30 19:31:44 +0200

    Hero object

--- src/app/app.js
+++ src/app/app.js
@@ -9,16 +9,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+//noinspection TypeScriptCheckImport
 var angular2_1 = require('angular2/angular2');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+})();
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = 'Windstorm';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app',
-            template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+            template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
