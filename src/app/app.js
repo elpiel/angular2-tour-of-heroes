71769e760fd669d6aac041511f9b9043025de205
Lachezar Lechev <luudman@gmail.com>
2015-11-30 19:52:03 +0200

    List of heroes

--- src/app/app.js
+++ src/app/app.js
@@ -23,15 +23,30 @@ var AppComponent = (function () {
             id: 1,
             name: 'Windstorm'
         };
+        this.heroes = HEROES;
     }
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app',
-            template: "\n      <h1>{{title}}</h1>\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>Name: </label>\n        <input value=\"{{hero.name}}\" placeholder=\"name\">\n      </div>\n    "
+            template: "\n      <h1>{{title}}</h1>\n      <h2>My Heroes</h2>\n       <ul class=\"heroes\">\n        <li *ng-for=\"#hero of heroes\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n        </li>\n       </ul>\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>Name: </label>\n        <input [(ng-model)]=\"hero.name\" placeholder=\"name\">\n      </div>\n    ",
+            styles: ["\n  .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}\n  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }\n  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}\n  .heroes .badge {\n    font-size: small;\n    color: white;\n    padding: 0.1em 0.7em;\n    background-color: #369;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -1px;\n  }\n  .selected { background-color: #EEE; color: #369; }\n  "],
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.NgFor]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 })();
 angular2_1.bootstrap(AppComponent);
+var HEROES = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
 //# sourceMappingURL=app.js.map
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 //noinspection TypeScriptCheckImport
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
 class Hero {
     id:number;
     name:string;
@@ -9,13 +9,38 @@ class Hero {
     selector: 'my-app',
     template: `
       <h1>{{title}}</h1>
+      <h2>My Heroes</h2>
+       <ul class="heroes">
+        <li *ng-for="#hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+       </ul>
       <h2>{{hero.name}} details!</h2>
       <div><label>id: </label>{{hero.id}}</div>
       <div>
         <label>Name: </label>
-        <input value="{{hero.name}}" placeholder="name">
+        <input [(ng-model)]="hero.name" placeholder="name">
       </div>
-    `
+    `,
+
+    styles: [`
+  .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+  .heroes .badge {
+    font-size: small;
+    color: white;
+    padding: 0.1em 0.7em;
+    background-color: #369;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -1px;
+  }
+  .selected { background-color: #EEE; color: #369; }
+  `],
+
+    directives: [FORM_DIRECTIVES, NgFor]
 })
 
 class AppComponent {
@@ -24,5 +49,20 @@ class AppComponent {
         id: 1,
         name: 'Windstorm'
     };
+    public heroes = HEROES;
 }
+
 bootstrap(AppComponent);
+
+var HEROES: Hero[] = [
+    {"id": 11, "name": "Mr. Nice"},
+    {"id": 12, "name": "Narco"},
+    {"id": 13, "name": "Bombasto"},
+    {"id": 14, "name": "Celeritas"},
+    {"id": 15, "name": "Magneta"},
+    {"id": 16, "name": "RubberMan"},
+    {"id": 17, "name": "Dynama"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
